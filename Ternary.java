6458e9202657c469165450fc39b95bf15aac548d
Ayushashverma <ayushvermaash@gmail.com>
2024-07-22 16:04:05 +0530

    ternary pushed

---
+++ Ternary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Ternary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        String string =(age>=18)?"Eligible ":"Not eligible";
+        System.out.println(string);
+
+
+
+
+        
+        
+    }
+
+    
+}
