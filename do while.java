84cf34f18752b0cbef5c7f3c3f25962528867ae9
Ayushashverma <ayushvermaash@gmail.com>
2024-07-22 13:57:06 +0530

    addded first program

---
+++ do while.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class dowhileloop{
+    public static void main(String[] args) {
+        int i=50;
+        do{
+        System.out.println("hascsdjcjaskc");
+        }while(i<20);
+    }
+}
